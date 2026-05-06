fbc5864f8783f66ea600bef4f4d86daf76e675a2
PedroSodreUFES <pedro.malini@edu.ufes.br>
2026-05-06 15:29:52 -0300

    feat: setup polynomial regressor

--- poly_model.py
+++ poly_model.py
@@ -1,4 +1,13 @@
+import joblib
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, root_mean_squared_error
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
 # 1: Carregar Dados
 df_receita = pd.read_csv("./dataset/sales_data.csv")
@@ -6,4 +15,7 @@ print(df_receita.head())
 
 # 2: Treinamento do modelo
 graus = [1,2,3,4,5,6,7,8,9,10]
-# graus=[x]
+# graus=[8]
+
+for grau in graus:
+
